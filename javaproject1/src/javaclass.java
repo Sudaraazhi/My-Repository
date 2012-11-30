0aad9333f2974ff1727ecdad6e902e9165b14423
Sudaraazhi <sudaraazhi@mangotechno.com>
2012-11-30 17:25:08 +0530

    changes done

---
+++ javaproject1/src/javaclass.java
@@ -0,0 +1,12 @@
+
+public class javaclass {
+	public static void  main(String[] args) {
+		System.out.println("Welcome to my java applications");
+		int sum=0;
+		for(int i=0;i<=10;i++){
+			sum = sum+1;
+		}
+		System.out.println("Sum = "+sum);
+	}
+}
+	
